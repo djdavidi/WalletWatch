85caa8f55384f23d95bdf65e9eaf7d5fb702c066
[David Scherban] <davidscherban@gmail.com>
2018-02-06 16:26:56 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Wallet from "./wallet/Wallet"
 
-const fakeUserData = {url: "https://blockchain.info/rawaddr/1Mz7153HMuxXTuR2R1t78mGSdzaAtNbBWX", type: "Bitcoin"}
+const fakeUserData = {
+	address: "1Mz7153HMuxXTuR2R1t78mGSdzaAtNbBWX",
+	baseUrl: "https://blockchain.info/multiaddr?cors=true&n=100&active=",
+	type: "Bitcoin"
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Wallet url={fakeUserData.url} type={fakeUserData.type}></Wallet>
+          <Wallet user={fakeUserData}></Wallet>
       </div>
     );
   }
--- src/wallet/Wallet.js
+++ src/wallet/Wallet.js
@@ -1,32 +1,84 @@
 import React, {Component} from 'react';
-import axios from "axios"
+// import HeaderTag from "./HeaderTag"
 class Wallet extends Component {
-	// constructor() {
-	// 	super();
-	// 	this.state = {
-	// 		url:
-	// 	}
-	// }
+	constructor() {
+		super();
+		this.state = {
+			data: {},
+			txs: []
+		}
+	}
 	componentDidMount() {
-		axios.get(this.props.url)
+		// could implement fetch indefinite tx length
+		// by checking if txs < wallet.n_tx and going until have all
+		// but not doing it for the purposes of this exercise
+		let url = this.props.user.baseUrl + this.props.user.address
+		console.log(url)
+		fetch(url, { method: 'GET',
+				mode: "cors",
+               cache: 'default' })
 		.then(res => {
-			console.log("first res", res)
-
 			return res.json()
 		})
-		.then(rese => {
-			console.log("RES",rese)
-			// console.log("rese", res.json())
+		.then(response => {
+			this.setState({data: response});
+			this.setState({txs: response.txs});
+			console.log(this.state.data);
+			this.connection = new WebSocket("wss://ws.blockchain.info/inv");
+
+			this.connection.onopen = () => this.connection.send({"op":"addr_sub", "addr": this.props.user.address})
+			this.connection.onmessage = evt => {
+				this.setState({
+					txs: evt.data.concat(this.state.txs)
+				})
+			}
 		});
 	}
 
 	render() {
+		function convertSatToBit(num) {
+			return `${num/100000000} BTC`;
+		}
+		function convertUnixToDate(timestamp){
+			// in a real project would use moment.js
+			return new Date(timestamp * 1000);
+		}
 		return (
-			<div>Hello {this.props.url}</div>
+			<div className="wallet-container">
+				<div className="wallet-header">
+				
+				<div><span>ADDRESS</span> : {this.props.user.address}</div>
+				<div><span>BALANCE</span> : {this.state.data.wallet ? convertSatToBit(this.state.data.wallet.final_balance): ""}</div>
+				</div>
+				<div className="user-table">
+					<div className="table-header">
+						<div><span>From</span></div>
+						<div><span>Amount</span></div>
+						<div><span>Date</span></div>
+					</div>	
+					<div className="wallet-transactions">
+					{ this.state.txs ? (
+						this.state.txs.map(elem =>
+							<div key={elem.hash}>
+								<div>
+									{elem.result > 0 ? elem.inputs[0].prev_out.addr : elem.out[0].addr}
+								</div>
+								<div className={elem.result > 0 ? "gain" : "loss"}>
+									{convertSatToBit(elem.result)}
+								</div>
+								<div>
+									{convertUnixToDate(elem.time).toUTCString()}
+								</div>
+							</div>
+						)) : (
+						<div>empty</div>
+					)}
+					</div>
+				</div>
+			</div>
 		);
 	}
 }
-
 
 export default Wallet;
 
